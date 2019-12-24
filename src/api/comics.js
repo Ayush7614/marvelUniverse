ff7444089990148601c30d5525cfe8536fae969b
Vishalghyv <vishal38785@gmail.com>
2019-12-25 03:45:30 +0530

    Adding api for comic

--- src/api/comics.js
+++ src/api/comics.js
@@ -1,8 +1,8 @@
 import apiConfig from './api-config';
 
-const fetchComics = page => {
+const fetchComicsByTitle = (title,page) => {
   return new Promise((resolve, reject) => {
-    fetch(`https://gateway.marvel.com/v1/public/comics?ts=${apiConfig.timestamp}&apikey=${apiConfig.marvelapikey}&hash=${apiConfig.marvelapihash}&limit=${apiConfig.perPage}&offset=${(page - 1) * apiConfig.perPage}`)
+    fetch(`https://gateway.marvel.com:443/v1/public/comics?title=${title}&apikey==${apiConfig.marvelapikey}`)
       .then(res => res.json())
       .then(res => {
         resolve(res.data);
